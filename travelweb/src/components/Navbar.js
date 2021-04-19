02cf725e2f0bf324a3e6f5af6d8d4df368f924df
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-04-19 17:31:39 +0700

    Update Navbar.js
    useeffect

--- travelweb/src/components/Navbar.js
+++ travelweb/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
@@ -19,6 +19,11 @@ function Navbar() {
       };
     
       window.addEventListener('resize', showButton);
+
+      useEffect(() => {
+        showButton();
+      }, []);
+    
 
 
 
